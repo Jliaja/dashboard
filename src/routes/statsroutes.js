f1832548cc87ae28c6568ca604cd0455d179cfb2
Jli <Fazlinurfahmi92@gmail.com>
2026-02-17 18:04:53 +0700

    fix missing statsController

--- src/routes/statsroutes.js
+++ src/routes/statsroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getStats } = require('../controllers/statsController');
+const { getStats } = require('../controllers/statscontroller');
 
 router.get('/', getStats);
 
